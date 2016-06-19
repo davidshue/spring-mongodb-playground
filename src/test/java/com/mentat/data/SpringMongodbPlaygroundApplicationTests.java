c908f3c95fd41060970ba831272dac6902406a98
David Shue <david.shue@babycenter.com>
2016-06-19 14:24:22 -0700

    tests added

--- src/test/java/com/mentat/data/SpringMongodbPlaygroundApplicationTests.java
+++ src/test/java/com/mentat/data/SpringMongodbPlaygroundApplicationTests.java
@@ -1,18 +1,28 @@
 package com.mentat.data;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.OutputCapture;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringMongodbPlaygroundApplication.class)
 @WebAppConfiguration
 public class SpringMongodbPlaygroundApplicationTests {
 
+	@ClassRule
+	public static OutputCapture outputCapture = new OutputCapture();
+
 	@Test
-	public void contextLoads() {
+	public void testDefaultSettings() throws Exception {
+		assertThat(outputCapture.toString(), containsString("firstName:Alice, lastName:Smith"));
 	}
 
 }
